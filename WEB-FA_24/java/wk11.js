febded2f0491940847e720d3aba4e7e99bacc995
jojo5451 <119837626+jojo5451@users.noreply.github.com>
2024-11-06 17:13:23 -0700

    icaw11

--- WEB-FA_24/java/wk11.js
+++ WEB-FA_24/java/wk11.js
@@ -1,71 +1,73 @@
-hello();
 
-function hello(){
-    alert("hello " + name);
+
+
+function even(n) {
+    return n % 2 === 0;
 }
-
-function even(n){
-    if(n%2 == 0){
-        return true;
-    }else{
-        return false;
-    }
-}
-
 console.log(even(20));
 console.log(even(1));
 console.log(even(13));
 
 
-let text =["one","two", "three"];
+let text = ["one", "two", "three"];
 let paragraph = "";
 
-function buildText(arr){
-    for(let i=0; i<arr.length; i++){
+function buildText(arr) {
+    paragraph = ""; 
+    for (let i = 0; i < arr.length; i++) {
         console.log(arr[i]);
         paragraph += arr[i] + " ";
     }
-    document.getElementById("my_para").innerHTML = paragraph;
-
+    document.getElementById("my_para").innerText = paragraph;
 }
-document.getElementById("button_1").addEventListener("click", function(e){buildText(text)});
 
 
-function tellFortune(numofChildren,partnerName,geographicLocation,jobTitle){
+document.getElementById("button_1").addEventListener("click", function () {
+    buildText(text);
+});
+
+function tellFortune(numofChildren, partnerName, geographicLocation, jobTitle) {
     let fortune = `You will be a ${jobTitle} in ${geographicLocation}, and married to ${partnerName} with ${numofChildren} kids.`;
-    document.write(fortune + "<br>");
+    document.getElementById("my_id").innerHTML += `<p>${fortune}</p>`;
 }
-
 tellFortune(2, "Alex", "New York", "Engineer");
 tellFortune(3, "Jamie", "Tokyo", "Designer");
 tellFortune(1, "Sam", "Paris", "Chef");
 
+
 function calculateDogAgeFromInput() {
     let humanAge = parseInt(prompt("Enter your dog's age in human years:"));
-    let dogAge = humanAge * 7;
-    document.write(`Your doggie is ${dogAge} years old in dog years!<br>`);
+    if (!isNaN(humanAge)) {
+        let dogAge = humanAge * 7;
+        document.getElementById("my_id").innerHTML += `<p>Your doggie is ${dogAge} years old in dog years!</p>`;
+    } else {
+        alert("Please enter a valid number.");
+    }
+}
+
+
+function reverseNumber(num) {
+    let reversed = num.toString().split("").reverse().join("");
+    document.getElementById("my_id").innerHTML += `<p>Reversed number: ${reversed}</p>`;
+}
+
+
+function alphabetizeString(str) {
+    let sortedStr = str.split("").sort().join("");
+    document.getElementById("my_id").innerHTML += `<p>Alphabetized string: ${sortedStr}</p>`;
+}
+
+
+function capitalizeWords(sentence) {
+    let capitalized = sentence
+        .split(" ")
+        .map(word => word.charAt(0).toUpperCase() + word.slice(1))
+        .join(" ");
+    document.getElementById("my_id").innerHTML += `<p>Capitalized sentence: ${capitalized}</p>`;
 }
 
 
 calculateDogAgeFromInput();
-
-function reverseNumber(num) {
-    let reversed = num.toString().split("").reverse().join("");
-    document.write(`Reversed number: ${reversed}<br>`);
-}
-
-reverseNumber(32243);
 reverseNumber(12345);
-
-function alphabetizeString(str) {
-    let sortedStr = str.split("").sort().join("");
-    document.write(`Alphabetized string: ${sortedStr}<br>`);
-}
-
-
-alphabetizeString("webmaster");
 alphabetizeString("javascript");
-
-
-
-
+capitalizeWords("hello world");
