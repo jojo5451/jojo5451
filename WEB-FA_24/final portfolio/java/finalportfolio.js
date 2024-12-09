d337d205d3a8965d23254f02912e944b162a6782
jojo5451 <119837626+jojo5451@users.noreply.github.com>
2024-12-09 15:24:04 -0700

    Final

---
+++ WEB-FA_24/final portfolio/java/finalportfolio.js
@@ -0,0 +1,17 @@
+fetch("projects.json")
+  .then(response => response.json())
+  .then(projects => {
+    const projectsContainer = document.querySelector(".projects-container");
+    projects.forEach(project => {
+      const projectCard = `
+        <div class="project-container project-card">
+          <img src="${project.image}" alt="${project.title}" class="project-pic" />
+          <h3 class="project-title">${project.title}</h3>
+          <p class="project-details">${project.description}</p>
+          <a href="${project.link}" target="_blank" class="project-link">Check it Out</a>
+        </div>`;
+      projectsContainer.innerHTML += projectCard;
+    });
+  })
+  .catch(error => console.error("Error loading projects:", error));
+  
